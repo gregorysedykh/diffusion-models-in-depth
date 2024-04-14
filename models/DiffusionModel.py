402478d3dee50957d59c10c2834efe5c8bb99216
Gregory Sedykh <62842044+gregorysedykh@users.noreply.github.com>
2024-04-14 16:57:31 +0200

    implemented simple DDPM for MNIST missing self-attention

--- models/DiffusionModel.py
+++ models/DiffusionModel.py
@@ -1,45 +1,85 @@
 import torch
 import torch.nn as nn
-from torchvision import transforms
-from matplotlib import pyplot as plt
-from utils import utils
+import torch.nn.functional as F
 
-import numpy as np
+class DiffusionModel:
+    def __init__(self, timesteps: torch.Tensor, model, device):
+        """
+        Args:
+            timesteps: torch.Tensor - tensor of shape (timesteps,) containing the number of timesteps
+            model: nn.Module - model to be used for the diffusion process
+            device: torch.device - device to be used
+        """
 
-class DiffusionModel(nn.Module):
-
-    def __init__(self, device, timesteps=1000):
-        super().__init__()
         self.device = device
+        self.model = model.to(self.device)
         self.timesteps = timesteps
 
-    def forward(self, x_0):
+        # Forward process parameters
+        self.beta_linear_schedule = torch.linspace(10**(-4), 0.02, timesteps.shape[0]).to(self.device)
+        self.alpha_schedule = 1 - self.beta_linear_schedule
+        self.alpha_prod = torch.cumprod(self.alpha_schedule, dim=0)
+    
+        self.sigma2 = self.beta_linear_schedule
+
+
+    def forward_process(self, x_0, t, noise=None):
         """
-        Adds Gaussian noise to the input image using cosine scheduler
+        Input:
+            x_0: tensor of shape (batch_size, 3, H, W) - image to be noised
+            t: int - timestep
+        Output:
+            x_t: tensor of shape (batch_size, 3, H, W) - noised image for the given timestep t
         """
-        f = utils.cosine_scheduler
-        s = 0.008
-        alpha_prod = f(self.timesteps, self.timesteps, s) / f(0, self.timesteps, s)
-        alpha_prod = torch.tensor(alpha_prod, dtype=torch.float32)
-
-        # Check device for x_0
-        if x_0.device != self.device:
-            x_0 = x_0.to(self.device)
-
-        mu = torch.sqrt(alpha_prod) * x_0
-        noise = torch.sqrt(1 - alpha_prod) * torch.randn_like(x_0)
-
-        x_t = mu + noise
+        mean = torch.sqrt(self.alpha_prod[t]) * x_0
+        std = torch.sqrt(1 - self.alpha_prod[t])
+        if noise is None:
+            noise = torch.randn_like(x_0) * std
+        x_t = mean + noise * std
 
         return x_t
-    
-    def display_forward(self, x_0):
-        x_t = self.forward(x_0)
-        plt.imshow(x_t)
-        
 
-    def backward(self):
+
+    def loss(self, x_0, t, eps):
         """
-        Using the U-Net, the model will try to remove the noise from the image
+        Input:
+            x_0: tensor of shape (batch_size, 3, H, W) - image
+            t: tensor of shape (batch_size,) - timestep
+            eps: tensor of shape (batch_size, 3, H, W) - noise
+        Output:
+            returns the loss for the given timestep t
         """
-        pass
+        x_t = self.forward_process(x_0, t, eps)
+        loss = F.mse_loss(self.model(x_t, t), eps)
+
+        return loss
+    
+
+    def sample(self):
+        """
+        Samples a random image from the model.
+        """
+        self.model.eval()
+        with torch.no_grad():
+            x_t = torch.randn(64, 1, 32, 32).to(self.device)
+
+            for t in reversed(range(self.timesteps.shape[0])):
+                if t > 1:
+                    z = torch.randn_like(x_t)
+                else:
+                    z = torch.zeros_like(x_t)
+
+                # Compute scaling factors and noise contributions
+                sqrt_alpha_t_inv = 1 / torch.sqrt(self.alpha_schedule)
+                sqrt_one_minus_alpha_prod_t = torch.sqrt(1 - self.alpha_prod)
+                sqrt_beta_t = torch.sqrt(self.sigma2)
+
+                # Predict noise using the model
+                predicted_noise = self.model(x_t, torch.tensor([t-1]))
+
+                # Denoise step
+                x_t = sqrt_alpha_t_inv[t-1] * (x_t - (1 - self.alpha_schedule[t-1])/(sqrt_one_minus_alpha_prod_t[t-1]) * predicted_noise) + sqrt_beta_t[t-1] * z
+
+            return x_t
+
+
--- models/UNet.py
+++ models/UNet.py
@@ -1,11 +1,202 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+def time_embedding(timesteps: torch.Tensor, time_dim: int) -> torch.Tensor:
+    """
+    Sinusoidal time embeddings
+    Args:
+        timesteps: tensor of shape (batch_size,) - time steps
+        time_dim: int - dimension of the time embedding
+    Returns:
+        tensor of shape (batch_size, time_dim) - time embeddings
+    """
+    denom = torch.arange(1, time_dim // 2 + 1, dtype=torch.float32)
+    sin = torch.sin(timesteps[:, None] / 10000 ** (2 * (denom - 1) / time_dim))
+    cos = torch.cos(timesteps[:, None] / 10000 ** (2 * (denom - 1) / time_dim))
+    emb = torch.cat([sin, cos], dim=-1)
+
+    return emb
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, time_dim, dropout=0.1):
+        super().__init__()
+
+        self.time_embed = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(time_dim, out_channels)
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.gn1 = nn.GroupNorm(8, in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+        self.gn2 = nn.GroupNorm(8, out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+        if in_channels != out_channels:
+            self.residual = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        else:
+            self.residual = nn.Identity()
+
+
+    def forward(self, x, t):
+        out = x
+
+        out = self.gn1(out)
+        out = F.silu(out)
+        out = self.conv1(out)
+        
+        out = out + self.time_embed(t)[:, :, None, None]
+
+        out = self.gn2(out)
+        out = F.silu(out)
+        out = self.dropout(out)
+        out = self.conv2(out)
+
+        out = out + self.residual(x)
+
+        return out
+    
+
+class DownsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        """
+        Upsamples from in_channels to out_channels
+
+        Args:
+            in_channels: int - number of input channels
+            out_channels: int - number of output channels
+        """
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x, t):
+        return self.conv(x)
+    
+        
+    
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        """
+        Downsamples from in_channels to out_channels
+
+        Args:
+            in_channels: int - number of input channels
+            out_channels: int - number of output channels
+        """
+        super().__init__()
+        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x, t):
+        return self.conv(x)
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.q = nn.Conv2d(dim, dim, kernel_size=1)
+        self.k = nn.Conv2d(dim, dim, kernel_size=1)
+        self.v = nn.Conv2d(dim, dim, kernel_size=1)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x, t):
+        batch_size, channels, height, width = x.shape
+        q = self.q(x).view(batch_size, channels, -1)
+        k = self.k(x).view(batch_size, channels, -1)
+        v = self.v(x).view(batch_size, channels, -1)
+        attn = torch.bmm(q.permute(0, 2, 1), k)
+        attn = self.softmax(attn)
+        out = torch.bmm(v, attn.permute(0, 2, 1))
+        return out.view(batch_size, channels, height, width)
 
 
 class UNet(nn.Module):
-
-    def __init__(self):
+    def __init__(self, img_channels):
         super().__init__()
 
-    def forward(self):
-        pass
+        self.time_dim = 128
+
+        self.time_proj = nn.Sequential(
+            nn.Linear(self.time_dim, self.time_dim),
+            nn.SiLU(),
+            nn.Linear(self.time_dim, self.time_dim)
+        )
+
+        self.img_channels = img_channels
+
+        self.first_conv = nn.Conv2d(self.img_channels, 32, kernel_size=3, stride=1, padding=1)
+
+        self.encoder = nn.ModuleList()
+
+        self.encoder.append(ResidualBlock(32, 32, self.time_dim))
+        # self.encoder.append(SelfAttention(32))
+        self.encoder.append(DownsampleBlock(32, 64))
+        self.encoder.append(ResidualBlock(64, 64, self.time_dim))
+        # self.encoder.append(SelfAttention(64))
+        self.encoder.append(DownsampleBlock(64, 128))
+        self.encoder.append(ResidualBlock(128, 128, self.time_dim))
+        # self.encoder.append(SelfAttention(128))
+        self.encoder.append(DownsampleBlock(128, 256))
+        self.encoder.append(ResidualBlock(256, 256, self.time_dim))
+        # self.encoder.append(SelfAttention(256))
+
+
+        self.bottleneck = nn.ModuleList()
+        self.bottleneck.append(ResidualBlock(256, 256, self.time_dim))
+        # self.bottleneck.append(SelfAttention(256))
+        self.bottleneck.append(ResidualBlock(256, 256, self.time_dim))
+
+        self.decoder = nn.ModuleList()
+        self.decoder.append(ResidualBlock(256, 256, self.time_dim))
+        # self.decoder.append(SelfAttention(256))
+        self.decoder.append(UpsampleBlock(256, 128))
+        self.decoder.append(ResidualBlock(256, 128, self.time_dim))
+        # self.decoder.append(SelfAttention(128))
+        self.decoder.append(UpsampleBlock(128, 64))
+        self.decoder.append(ResidualBlock(128, 64, self.time_dim))
+        # self.decoder.append(SelfAttention(64))
+        self.decoder.append(UpsampleBlock(64, 32))
+        self.decoder.append(ResidualBlock(64, 32, self.time_dim))
+        # self.decoder.append(SelfAttention(32))
+
+        self.final_norm = nn.GroupNorm(8, 32)
+        self.final_activation = nn.SiLU()
+        self.final_conv = nn.Conv2d(32, self.img_channels, kernel_size=3, padding=1)
+
+
+    def forward(self, x, t):
+        
+        out = self.first_conv(x)
+
+        time_emb = time_embedding(t, self.time_dim).to(x.device)
+        time_emb = self.time_proj(time_emb)
+
+
+        skips = []
+        for layer in self.encoder:
+            if isinstance(layer, DownsampleBlock):
+                skips.append(out)
+            out = layer(out, time_emb)
+        
+
+        for layer in self.bottleneck:
+            out = layer(out, time_emb)
+
+
+        for layer in self.decoder:
+            if isinstance(layer, UpsampleBlock):
+                out = layer(out, time_emb)
+                out = torch.cat([out, skips.pop()], dim=1)
+            else:
+                out = layer(out, time_emb)
+
+
+
+        out = self.final_norm(out)
+        out = self.final_activation(out)
+        out = self.final_conv(out)
+
+        return out
